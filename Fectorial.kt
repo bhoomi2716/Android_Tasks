96fac378756e26c5e21fd752a094e22fac1d1ac4
bhoomi2716 <bhoomiwadhvana@gmail.com>
2024-11-20 12:26:22 +0530

    commit

---
+++ Fectorial.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    var fect=1
+
+    var i=1
+    while(i<=5)
+    {
+        fect*=i
+        i++
+    }
+
+    print("Fectorial : ")
+    print(fect)
+}
---
+++ print51To60whileloop.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    var i=51
+    while(i<=60)
+    {
+        println(i)
+        i++
+    }
+}
