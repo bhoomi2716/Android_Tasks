3aebe384068733ed52172dd30b6df82966beef2d
bhoomi2716 <bhoomiwadhvana@gmail.com>
2024-11-22 10:48:07 +0530

    commit

--- 21-11-2024/ClassAndObject.kt
+++ 21-11-2024/ClassAndObject.kt
@@ -1,23 +1,34 @@
 class student
 {
-//    var enrollmentNo:
-    var Name=""
-//    var Age:
+    var enrollmentNo : Int ? = 0
+    var Name = ""
+    var Age : Int? = 0
 
     fun displayInfo()
     {
-//        println("Your Enrollment Number Is : $enrollmentNo")
+        println("Your Enrollment Number Is : $enrollmentNo")
         println("Your Name Is : $Name")
-//        println("Your Age Is : $Age")
+        println("Your Age Is : $Age")
     }
 }
 
 fun main()
 {
-    var Stu1=student()
-//    Stu1.enrollmentNo = 101
-    Stu1.Name="abc"
-//    Stu1.Age= 21
+    println("--------------------------------------------")
 
+    var Stu1=student()
+    Stu1.enrollmentNo = 101
+    Stu1.Name = "abc"
+    Stu1.Age = 21
     Stu1.displayInfo()
+
+    println("--------------------------------------------")
+
+    var Stu2=student()
+    Stu2.enrollmentNo = 102
+    Stu2.Name = "xyz"
+    Stu2.Age = 20
+    Stu2.displayInfo()
+
+    println("--------------------------------------------")
 }
